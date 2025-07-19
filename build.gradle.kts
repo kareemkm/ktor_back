02fa79d0568411d31901996b9a663faa2a5e5c26
Kareemkm <kareemabunaga@gmail.com>
2025-07-19 14:47:20 +0300

    initial ktor commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    kotlin("plugin.serialization") version "1.9.23"
+}
+
+group = "com.kareem"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+
+}
---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,60 @@
+package com.kareem
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+
+fun Application.configureRouting() {
+
+    install(ContentNegotiation){
+        json()
+    }
+
+    routing {
+        get("/kareem") {
+            call.respondText("kareem")
+        }
+        post("/kareem") {
+            val request = call.receive<User>()
+            call.respond(mapOf("message" to "Hi ${request.name}! You are ${request.age} years old."))
+            call.respond(mapOf("state" to "success"))
+        }
+        get("/user") {
+            val  users = listOf(
+                User(
+                    id = 1,
+                    name = "kareem",
+                    age = 23,
+                    city = "cairo",
+                ),
+                User(
+                    id = 2,
+                    name = "mustafa",
+                    age = 54,
+                    city = "dearness",
+                ),
+                User(
+                    id = 3,
+                    name = "ahmed",
+                    age = 120,
+                    city = "kkk",
+                )
+            )
+            call.respond(users)
+        }
+    }
+}
+
+
+@Serializable
+data class User(
+    val id: Int = 1,
+    val name: String,
+    val age: Int,
+    val  city: String = "kareem",
+)
